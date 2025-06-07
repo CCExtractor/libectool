6a1aa5438301bf84cfbeea4a008ae7e187ff4842
AhmedYasserrr <ahmed.gamea@ejust.edu.eg>
2025-06-07 07:18:20 -0700

    refactor: declare ascii_mode in libectool header

--- src/core/libectool.h
+++ src/core/libectool.h
@@ -1,6 +1,8 @@
 #ifndef LIBECTOOL_H
 #define LIBECTOOL_H
 
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -15,6 +17,9 @@ void pause_fan_control();
 void set_fan_speed(int speed);
 float get_max_temperature();
 float get_max_non_battery_temperature();
+
+/* ASCII mode for printing, default off */
+extern int ascii_mode;
 
 #ifdef __cplusplus
 }
